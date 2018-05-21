aa298efc4d3cdc16eae97eef4774e62e5b0878cd
rahmadoktakhoirul1008 <38611266+rahmadoktakhoirul1008@users.noreply.github.com>
2018-05-22 00:11:41 +0700

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -1,1 +1,12 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+	
+	char back;
+   int n , pilih;
+  menu:
+   cout<<"1.selection sort"<<endl;
+   cout<<"2.bubble sort"<<endl;
+   cout<<"3.merge sort"<<endl;
+   cout<<"PILIHAN ANDA : ";cin>>pilih;

eb436c738937a5003a9d8f3e8053c7f141a7e8c5
ArifPebriansyah1014 <38611082+ArifPebriansyah1014@users.noreply.github.com>
2018-05-22 00:18:40 +0700

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -10,3 +10,33 @@ int main(){
    cout<<"2.bubble sort"<<endl;
    cout<<"3.merge sort"<<endl;
    cout<<"PILIHAN ANDA : ";cin>>pilih;
+
+	int a, b, c, simpan;
+    		int data[100];
+  
+  
+   switch (pilih) {
+   	
+   case 1 : 
+			system ("cls");
+ 
+  
+    cout<<"jumlah angka yang di input : "; cin>>n;
+    
+	for(a=0;a<n;a++)
+    {
+        cout<<"Masukkan nilai "<<a+1<<" : ";cin>>data[a];
+    }
+    for(a=0;a<n-1;a++)
+    {
+        for(b=0;b<n;b++)
+        {
+            if(data[b]<data[c])
+            {
+          
+            }
+        }
+        simpan=data[c];
+        data[c]=data[a];
+        data[a]=simpan;
+    }
